7b89a68eb9b54f5cc1abb4a18b6aea517e4973bc
Smahito Ja <actionsisty@gmail.com>
2022-01-27 12:04:17 +0700

    demo success

--- serverSocket.mjs
+++ serverSocket.mjs
@@ -3,39 +3,6 @@ import net from "net";
 import { EventEmitter } from "events";
 const myEmitter = new EventEmitter();
 
-// const server = net.createServer((socket) => {
-//   console.log("connected:! id", socket.id);
-//   socket.on("data", (data) => {
-//     console.log("data:", data.toString());
-
-//     switch (data.toString().trim()) {
-//       case "h":
-//         socket.write("Hello, world!\n");
-//         server.emit("hello");
-//         //myEmitter.emit("hello");
-//         break;
-//       case "m":
-//         socket.write("You get Machine\n");
-//         socket.emit("machine", "Broadcast You get Machine");
-//         break;
-//     }
-//   });
-
-//   socket.on("hello", () => {
-//     console.log("hello");
-//   });
-//   socket.on("machine", (m) => {
-//     console.log("Machin", m);
-//   });
-// });
-
-//set MyEmitter
-// myEmitter.on("hello", () => {
-//   console.log("myEmitter hello");
-// });
-
-// server.listen(88);
-
 
 // const io = require("socket.io").listen(server);
 const io = new Server()
@@ -52,8 +19,5 @@ io.on("connection", (socket) => {
       io.emit("hello", ["emit event Hello", data]);
     });
 });
-
-
-
 
 io.listen(88);
